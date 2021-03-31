6d2c85902ee2fa873860b77469a584f5b2bc1319
jitendra.neepanlok@gmail.com <jitendra.neepanlok@gmail.com>
2021-03-31 17:58:05 +0530

    Api setUp done

--- app/src/main/java/com/ltg/ltgfresh/Activity/LoginActivity.java
+++ app/src/main/java/com/ltg/ltgfresh/Activity/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.ltg.ltgfresh.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatEditText;
 import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
@@ -12,8 +14,14 @@ import android.widget.RelativeLayout;
 
 import com.ltg.ltgfresh.R;
 
+import java.util.regex.Pattern;
+
 public class LoginActivity extends AppCompatActivity {
-RelativeLayout rl_register;
+    RelativeLayout rl_register;
+    AppCompatEditText et_phone, et_password;
+    AppCompatButton btn_login;
+    String contact_no, passwprd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,12 +37,55 @@ RelativeLayout rl_register;
     }
 
     private void initView() {
-        rl_register = (RelativeLayout)findViewById(R.id.rl_register);
+        rl_register = (RelativeLayout) findViewById(R.id.rl_register);
         rl_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
             }
         });
+
+        btn_login = (AppCompatButton) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkValidation();
+            }
+        });
+    }
+
+    private void checkValidation() {
+        et_phone = (AppCompatEditText) findViewById(R.id.et_phone);
+        et_password = (AppCompatEditText) findViewById(R.id.et_password);
+        passwprd = et_password.getText().toString();
+        if (isValidPhone(contact_no = et_phone.getText().toString())) {
+            if (!passwprd.equals("")) {
+                CallApi();
+            } else {
+                et_password.setError("Enter your Password");
+            }
+
+        } else {
+            et_phone.setError("Enter valid Number");
+        }
+    }
+
+    private boolean isValidPhone(String phone) {
+        boolean check = false;
+        if (!Pattern.matches("[0-10]+", phone)) {
+            if (phone.length() < 10) {
+                check = false;
+            } else {
+                check = true;
+            }
+        } else {
+            check = false;
+        }
+        return check;
+    }
+
+    private void CallApi() {
+
+
     }
 }
---
+++ app/src/main/java/com/ltg/ltgfresh/Network/ApiInterface.java
@@ -0,0 +1,17 @@
+package com.ltg.ltgfresh.Network;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
+public interface ApiInterface {
+
+   /* @POST("registration")
+    Call<RegistrationResponse> postSignUp(@Header("Content-Type") String content_type, @Body RegistrationModel registrationModel);
+
+    @POST("login")
+    Call<LoginResponseMsg> postSignIn(@Header("Content-Type") String content_type, @Body LoginModel loginModel);*/
+
+
+}
